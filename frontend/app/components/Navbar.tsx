9d6749bcecba6c7f860a83ceffaecb91599ce570
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-22 10:41:25 +0000

    backend Express + Ts setup

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+export default function Navbar() {
+    return (
+        <section className="h-20 w-screen bg-transparent text-[#ededed] container p-10">
+            <p className=" font-extrabold ">Collab Study</p>
+        </section>
+    )
+}
+
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/Navbar";
+
 
 export default function Home() {
   return (
-   <></>
-  )
+    <section className="bg-[#0e0e11] h-screen w-screen">
+      <Navbar />
+    </section>
+  );
 }
