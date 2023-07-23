2aaa4fc8e0c8d30b33e07458946ff3cd02fa0715
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-23 13:45:35 +0000

    added Navbar elements

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,9 +1,23 @@
 
 export default function Navbar() {
     return (
-        <section className="h-20 w-screen bg-transparent text-[#ededed] container p-10">
-            <p className=" font-extrabold ">Collab Study</p>
-        </section>
-    )
+      <section className="h-20 w-full bg-transparent text-[#ededed] px-10 py-10 flex align-center justify-between">
+        <p className=" font-extrabold ">Collab Study</p>
+
+        <ul className="flex gap-10 text-[#a6a6a6]">
+          <li className="cursor-pointer hover:text-[#ededed] duration-300">
+            Intro
+          </li>
+          <li className="cursor-pointer hover:text-[#ededed] duration-300">
+            About
+          </li>
+          <li className="cursor-pointer hover:text-[#ededed] duration-300">
+            Contact
+          </li>
+        </ul>
+
+        <button className="flex align-center justify-center font-bold px-2 py-4 border border-solid border-white rounded">Login</button>
+      </section>
+    );
 }
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <section className="bg-[#0e0e11] h-screen w-screen">
+    <section className="bg-[#0e0e11] min-h-screen w-full font-roboto">
       <Navbar />
     </section>
   );
