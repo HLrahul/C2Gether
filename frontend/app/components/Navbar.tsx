6d5e5f520b3ca4ce6024233f105e0ef296e8ec0c
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-07-25 11:35:43 +0000

    making Navbar responsive

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,10 +1,10 @@
-
 export default function Navbar() {
-    return (
-      <section className="h-20 w-full bg-transparent text-[#ededed] px-10 py-10 flex align-center justify-between">
-        <p className=" font-extrabold ">Collab Study</p>
+  return (
+    <section className="h-16 w-full bg-transparent text-[#ededed] px-10 py-10 flex items-center justify-between">
+      <p className="text-xl font-extrabold">Collab Study</p>
 
-        <ul className="flex gap-10 text-[#a6a6a6]">
+      <div className="absolute min-h-[30vh]">
+        <ul className="flex md:flex-row flex-col gap-10 text-[#a6a6a6]">
           <li className="cursor-pointer hover:text-[#ededed] duration-300">
             Intro
           </li>
@@ -15,9 +15,19 @@ export default function Navbar() {
             Contact
           </li>
         </ul>
+      </div>
 
-        <button className="flex align-center justify-center font-bold px-2 py-4 border border-solid border-white rounded">Login</button>
-      </section>
-    );
+      <div className="flex gap-5">
+        <button className="flex items-center justify-center font-bold px-4 py-2 border border-solid border-white rounded-3xl">
+          Login
+        </button>
+
+        <div className="hidden rounded w-12 md-flex flex-col items-center justify-center gap-1 cursor-pointer">
+          <span className="block w-8 h-1 bg-slate-100 rounded"></span>
+          <span className="block w-8 h-1 bg-slate-100 rounded"></span>
+          <span className="block w-8 h-1 bg-slate-100 rounded"></span>
+        </div>
+      </div>
+    </section>
+  );
 }
-
