65af18b5a41a5c82ccfdf1539f3e92b5ea20d7d3
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-10-17 09:02:39 +0000

    implemented ˋJoinRoomAgainˋ

--- client/app/[roomid]/page.tsx
+++ client/app/[roomid]/page.tsx
@@ -1,23 +1,22 @@
 "use client"
 
 import { useEffect, useState } from "react"
-import { useParams, useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 import BottomBar from "@/components/BottomBar"
 import { useUserStore } from "@/store/userStore"
 import JoinRoomPrompt from "@/components/JoinRoomPrompt"
 import DisconnectedDialog from "@/components/Disconnected"
 
 export default function RoomPage () {
-    const router = useRouter()
-    const [showDialog, setShowDialog] = useState(false)
-    const [roomId, setRoomId] = useState<string>("")
-
     const user = useUserStore(state => state.user)
-    
-    
+
+    const router = useRouter()
+
+    const roomId = window.location.pathname.substring(1) 
+    const [showDialog, setShowDialog] = useState(false)
+
     useEffect(() => {
         if(!user) {
-          setRoomId(window.location.pathname.split("/")[1])
           if (roomId.length === 21) {
             setShowDialog(true);
           } else {
@@ -27,12 +26,12 @@ export default function RoomPage () {
     }, []);
 
     return (
-        <div>
-            <DisconnectedDialog />
-
-            <JoinRoomPrompt roomId={roomId} showDialog={showDialog} />
-
-            <BottomBar />
-        </div>
-    )
+      <div>
+        <DisconnectedDialog />
+        
+        <JoinRoomPrompt roomId={roomId} showDialog={showDialog} setShowDialog={setShowDialog} />
+        
+        <BottomBar />
+      </div>
+    );
 } 
--- client/components/JoinRoomPrompt.tsx
+++ client/components/JoinRoomPrompt.tsx
@@ -1,19 +1,16 @@
-"use client";
+"use client"
 
-import * as z from "zod";
-import { socket } from "@/lib/socket";
-import { Loader2 } from "lucide-react";
-import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import {
   Form,
@@ -23,20 +20,32 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
 import { joinRoomSchema } from "@/lib/validations/JoinRoomSchema";
+import { useUserStore } from "@/store/userStore";
+import { useMembersStore } from "@/store/membersStore";
+import { RoomJoinedData } from "@/types";
+import { socket } from "@/lib/socket";
+import { toast } from "./ui/use-toast";
+import { Loader2 } from "lucide-react";
+import { Separator } from "./ui/separator";
+
 type JoinRoomForm = z.infer<typeof joinRoomSchema>;
 
 type propsType = {
-    roomId: string;
-    showDialog: boolean;
-}
+  roomId: string;
+  showDialog: boolean;
+  setShowDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export default function JoinRoomPrompt(props: propsType) {
+  const setUser = useUserStore((state) => state.setUser);
+  const setMembers = useMembersStore((state) => state.setMembers);
 
-  const { roomId, showDialog } = props;
+  const router = useRouter();
+  const { roomId, showDialog, setShowDialog } = props;
 
   const [isLoading, setIsLoading] = useState(false);
+  const [isHomeLoading, setIsHomeLoading] = useState(false);
 
   const form = useForm<JoinRoomForm>({
     resolver: zodResolver(joinRoomSchema),
@@ -57,10 +66,34 @@ export default function JoinRoomPrompt(props: propsType) {
     });
   }, []);
 
+  useEffect(() => {
+    socket.on("room-joined", ({ user, members }: RoomJoinedData) => {
+      setUser(user);
+      setMembers(members);
+      setShowDialog(false);
+    });
+
+    function handleErrorMessage({ message }: { message: string }) {
+      toast({
+        title: "Failed to join room!",
+        description: message,
+      });
+    }
+
+    socket.on("room-not-found", handleErrorMessage);
+
+    socket.on("invalid-data", handleErrorMessage);
+
+    return () => {
+      socket.off("room-joined");
+      socket.off("room-not-found");
+      socket.off("invalid-data", handleErrorMessage);
+    };
+  }, [router, toast, setUser, setMembers]);
+
   return (
     <>
-      { showDialog && (
-        <Dialog>
+      <Dialog open={showDialog} onOpenChange={setShowDialog}>
         <DialogContent className="w-[90vw] max-w-[400px]">
           <DialogHeader className="pb-2">
             <DialogTitle>Join the room now!</DialogTitle>
@@ -84,19 +117,6 @@ export default function JoinRoomPrompt(props: propsType) {
                 )}
               />
 
-              <FormField
-                control={form.control}
-                name="roomId"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input placeholder="Room ID" defaultValue={roomId} {...field} />
-                    </FormControl>
-                    <FormMessage className="text-xs" />
-                  </FormItem>
-                )}
-              />
-
               <Button type="submit" className="mt-2">
                 {isLoading ? (
                   <Loader2 className="h-4 w-4 animate-spin" />
@@ -106,9 +126,20 @@ export default function JoinRoomPrompt(props: propsType) {
               </Button>
             </form>
           </Form>
+
+          <Separator />
+
+          <Button
+            onClick={(e) => {
+              setIsHomeLoading(true);
+              router.replace("/");
+              setIsHomeLoading(false);
+            }}
+          >
+            {isHomeLoading ? <Loader2 className="h-4 w-4 animate-spin" /> : "Home"}
+          </Button>
         </DialogContent>
       </Dialog>
-      )}
     </>
   );
 }
