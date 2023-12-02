172ea183d01d226992a1ca884c081fecdce86b09
Rahul Kumar S <86195461+HLrahul@users.noreply.github.com>
2023-12-02 03:48:47 +0000

    Update server URL in socket.ts

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SERVER =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-    ? "https://main--tourmaline-hamster-ff51f8.netlify.app/"
+    ? `${process.env.NEXT_PUBLIC_SERVER_URL}`
     : "https://probable-goggles-wjrjjj5qvwph9jqv-3001.app.github.dev/";
 
 export const socket = io(SERVER, { transports: ["websocket"] });
