ecd1c2d2f607749db64bcb517809dbb3f7056620
HLrahul <rahuls10org@gmail.com>
2024-10-12 04:09:53 +0530

    feat: backend update

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -3,6 +3,6 @@ import { io } from "socket.io-client";
 const SERVER =
   process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
     ? `${process.env.NEXT_PUBLIC_SERVER_URL}`
-    : "https://probable-goggles-wjrjjj5qvwph9jqv-3001.app.github.dev/";
+    : "https://c2gether-socket-server.onrender.com/";
 
 export const socket = io(SERVER, { transports: ["websocket"] });
